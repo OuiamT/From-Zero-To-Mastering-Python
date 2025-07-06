4c3464c94f3d886edb0ae3e01f04a3035cecf699
OuiamT <tarikouiam24@gmail.com>
2025-07-06 16:41:55 +0200

    Create Solve the first unit project.py

---
+++ Solve the first unit project.py
@@ -0,0 +1,1 @@
+This is a simple Python project that asks the user to enter their name and the type od content they want to create on YouTube, and then print a message suggesting their channel name.
