00d7b3c72c1cfa18857f737db2588c4f17faeb29
OuiamT <tarikouiam24@gmail.com>
2025-07-06 16:54:28 +0200

    This is a simple Python project that asks the user to enter their name and the type od content they want to create on YouTube, and then print a message suggesting their channel name.

--- Solve the first unit project.py
+++ Solve the first unit project.py
@@ -1,1 +1,16 @@
-This is a simple Python project that asks the user to enter their name and the type od content they want to create on YouTube, and then print a message suggesting their channel name.
+
+
+# Needed Output:
+"""
+Welcome to the YouTube channel Name Generator:
+What is your name?
+-> answer1
+What is your channel about?
+-> answer2
+You could name your channel: (answer2 with answer1)
+"""
+
+print("Welcome to the YouTube channel Name Generator:")
+name = input("What is your name?\n")
+channel_about = input("What is your channel about?\n")
+print("You could name your channel: ("+channel_about+ " with "+ name+")")
