74dc07ec08108355aeb2a528c33ae87840ea18fe
OuiamT <tarikouiam24@gmail.com>
2025-07-31 14:13:29 +0200

    Create Assignments of the 7 unit

---
+++ Assignments of the 7 unit/main.py
@@ -0,0 +1,29 @@
+# 1
+# Needed Output:
+# Hello and Welcome to my function!
+
+def my_function():
+    print("** Hello and Welcome to my function! **")
+
+my_function()
+print("-" * 20)
+print()
+
+
+# 2
+# Needed Output:
+# Welcome to the program
+# Press "Enter" to exit ...
+# Thanks for using the program. See you soon!
+
+def welcome():
+    print("Welcome to the program")
+
+def bye():
+    print("Thanks for using the program. See you soon!")
+
+welcome()
+input("Press \"Enter\" to exit ...")
+bye()
+print("-" * 20)
+print()
