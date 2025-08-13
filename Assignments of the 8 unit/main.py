6d1288ff892462cbf836dc07ffcb41590bdd995f
OuiamT <tarikouiam24@gmail.com>
2025-08-13 10:15:31 +0200

    Update

--- Assignments of the 8 unit/main.py
+++ Assignments of the 8 unit/main.py
@@ -1,1 +1,22 @@
-Commande ECHO activ‚e.
+# 1
+# Needed Output
+
+# We have "apple" like a random fruit
+# Please guess a letter: a
+# Right
+# Wrong
+# Wrong
+# Wrong
+# Wrong
+
+import random
+
+fruits = ("apple", "banana", "pear", "tangerine", "plum", "grapce", "watermelon")
+random_fruit = random.choice(fruits)
+choice_a_char = input("Please guess a letter:  ").lower()
+print()
+for character in random_fruit:
+    if character == choice_a_char:
+        print("Right")
+    else:
+        print("Wrong")
