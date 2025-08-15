a9906d2ed7c6dec247a9ce8210942092daf68651
OuiamT <tarikouiam24@gmail.com>
2025-08-15 23:17:25 +0200

    Update

--- Assignments of the 8 unit/main.py
+++ Assignments of the 8 unit/main.py
@@ -41,6 +41,7 @@ print()
 for _ in random_fruit:
     hight_fruit.append("_")  #Or hight_fruit += "_"
 print(hight_fruit)
+
 choice_a_char = input("Please guess a letter:  ").lower()
 
 if choice_a_char in random_fruit:
@@ -51,3 +52,52 @@ if choice_a_char in random_fruit:
     
 else:
     print("Try again ...")
+
+
+# 3
+# Needed Output
+
+# We have "apple" like a random fruit
+# ['_', '_', '_', '_', '_']     
+# Please guess a letter:  a     
+# ['a', '_', '_', '_', '_']
+# Please guess a letter:  p
+# ['a', 'p', 'p', '_', '_']
+# Please guess a letter:  l
+# ['a', 'p', 'p', 'l', '_']
+# Please guess a letter:  z
+# Try again ...
+# ['a', 'p', 'p', 'l', '_']
+# Please guess a letter:  e
+# The random fruit is: apple
+#   YOU WIN       
+
+
+import random
+
+hight_fruit = []
+
+fruits = ("apple", "banana", "pear", "tangerine", "plum", "grapce", "watermelon")
+random_fruit = random.choice(fruits)
+print()
+
+for _ in random_fruit:
+    hight_fruit.append("_")  #Or hight_fruit += "_"
+
+while random_fruit != "".join(hight_fruit):
+    print("\n",hight_fruit)
+    choice_a_char = input("Please guess a letter:  ").lower()
+
+    if choice_a_char in random_fruit:
+        for index in range(len(random_fruit)):
+            if choice_a_char == random_fruit[index]:
+                hight_fruit[index] = choice_a_char
+    else:
+        print("Try again ...")
+
+print("\nThe random fruit is: ", "".join(hight_fruit))
+print("""
+**************
+  YOU WIN
+**************
+""")
