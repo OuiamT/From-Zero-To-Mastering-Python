5b2c6850654e0ea63fd80f574a6c419bd919a259
OuiamT <tarikouiam24@gmail.com>
2025-09-20 18:16:01 +0200

    Update Assignments of the 9 unit

--- Assignments of the 9 unit/main.py
+++ Assignments of the 9 unit/main.py
@@ -1,12 +1,3 @@
-# function with parameters and arguments
-# def fct(parameter):
-#   print(parameter)
-
-# fct(argument)
-# positional arguments
-# ??!! the difference between methode and function
-# the methode index() (the difference between this and find())
-
 # 1
 
 # Input:
