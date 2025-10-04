b6f68c1b67aaae071f934b85304f70227a771d2d
OuiamT <tarikouiam24@gmail.com>
2025-10-04 21:06:28 +0200

    Update

--- Assignments of the 11 unit/main.py
+++ Assignments of the 11 unit/main.py
@@ -77,4 +77,72 @@ def greet_person(name):
     return f"Hello, {name}! How are you?" 
 
 print(greet_person(input("Enter your name:  ")))
+print("-" * 20)
+print()
 
+
+# 5
+# Needed Output:
+# Welcome {name} to the restaurant!
+# Order taken successfuuly.
+# Thank you {name}, for choosing our restaurant!
+    
+
+def take_order(name):
+    print(f"\nWelcome {name} to the restaurant!")
+    print("Order taken successfuuly.")
+    thank_customer(name)
+
+def thank_customer(name):
+    print(f"Thank you {name}, for choosing our restaurant!")
+
+take_order(input("What is your name:\t").lower())
+print("-" * 20)
+print()
+
+# 6
+# Needed Output:
+# Enter your base salary: example 3000
+# How much bonus did you get?: example 250
+# Final salary after 15% tax is: $4500.0
+
+def calculate_tax(base_salary):
+    return base_salary * 0.15
+
+def final_salary(base_salary, bonus):
+    total_salary = base_salary + bonus 
+    return total_salary - calculate_tax(base_salary)
+
+base_salary = float(input("Enter your base salary:  "))
+bonus = float(input("How much bonus did you get?  "))
+print(f"\nFinal salary after 15% tax is:\n${final_salary(base_salary, bonus)}")
+print("-" * 20)
+print()
+
+
+# 7
+# Needed Output:
+# Enter a user name
+# Enter your email
+# Checking email name ....
+# Check if email is valid or not
+
+
+def check_result(name, email):
+    if "@" in email and "." in email:
+        print(f"User '{name}' with email '{email}' successfully added.")
+    else:
+        print("Invalid email format. Registration failed.")
+
+import time
+
+user_name = input("Enter a user name:  ")
+email = input("Enter your email:  ")
+time.sleep(2)
+print("Checking email name ....")
+time.sleep(2)
+print("Validation email adress ....")
+time.sleep(3)
+check_result(user_name, email)
+print("-" * 20)
+print()
