bbb83d926c9812b0b492c17bc6657314b928dcec
OuiamT <tarikouiam24@gmail.com>
2025-10-09 17:48:32 +0200

    Update

--- Assignments of the 12 unit/main.py
+++ Assignments of the 12 unit/main.py
@@ -1,1 +1,30 @@
-Commande ECHO activ‚e.
+# 1
+# Needed Output:
+# user name is : [used local variable]
+# user name is : [used global variable]
+
+name = "Ibrahim"
+def info():
+    name = "Adel"
+    print(f"User name inside info is: {name}")
+
+info()
+print(f"User name is: {name}")
+print("-" * 20)
+print()
+
+# 2
+# Needed Output:
+# [name: local variable] [contry: global variable]
+
+user_contry = "Morocco"
+def information():
+    user_name = "Mohamed"
+    print(user_name,  user_contry)
+
+information()
+print("-" * 20)
+print()
+
+# 3
+# Needed Output:
