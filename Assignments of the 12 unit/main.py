dfcb75df001e81a8282c04c8e810c9f33b911cb1
OuiamT <tarikouiam24@gmail.com>
2025-10-23 12:56:10 +0200

    Update assignments of the 12 unit

--- Assignments of the 12 unit/main.py
+++ Assignments of the 12 unit/main.py
@@ -28,14 +28,36 @@ print()
 
 
 # 3
+# Needed Output:
+# Enter your number
+# All of messages are Errors or Exceptions (number egal 0, number not a one character or not a number).
+
+num = input("Enter your number:  ")
+if num == '0':
+    raise ValueError("Number Must Be Larger Than 0")
+
+elif len(num) != 1:
+    raise IndexError( "Only One Character Allowed")
+
+elif not num.isdigit():
+    raise Exception("Only Numbers Allowed")
+
+print(f"The number is: {num}")
+print("-" * 20)
+print()
+
+# 4
+
 
 while True:
     try:
-        age = int(input("Enter your age please: "))
-    except ValueError:
-        print("Sorry but your print is not an integer")
+        char = input("Add Letter From A to Z:  ")
+        if not char.isalpha():
+            print("The Letter Not In A - Z")
+        elif len(char) != 1:
+            print("You Must Write One Character Only")
+        else:
+            print(f"You typed {char}")
+            break
     except:
-        print("Find an Error")
-    else:
-        print(f"You born in: {2025 - age}")
-        break
+        print("Exist an Ettor")
