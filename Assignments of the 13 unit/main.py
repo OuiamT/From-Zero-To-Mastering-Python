0abf3d3c177063149bc086744c225657f2e3f584
OuiamT <tarikouiam24@gmail.com>
2025-10-24 18:16:37 +0200

    update

--- Assignments of the 13 unit/main.py
+++ Assignments of the 13 unit/main.py
@@ -1,1 +1,15 @@
-Commande ECHO activ‚e.
+# 1
+#* : Create a class.
+class Book:
+    def __init__(self, title, author, pages):
+        self.title = title
+        self.author = author
+        self.pages = pages
+
+#* : Create an object.
+book1 = Book("Origin", "Dan Brown", 542)
+book2 = Book("Still Alice", "Lisa Genova", 188)
+
+#* : Print title of book1 and author of book2.
+print(f"The one book's title is: {book1.title}")
+print(f"The two book's author is: {book1.author}")
