facbd92cb162efe6c9dbb521dc09ce8ef9fc26f8
OuiamT <tarikouiam24@gmail.com>
2025-11-29 16:09:30 +0100

    Update

--- Assignments of the 15 unit/main.py
+++ Assignments of the 15 unit/main.py
@@ -2,20 +2,27 @@ from turtle import Turtle, Screen
 window = Screen()
 window.setup(width=600, height=600)
 window.bgcolor("black")
-position = ((-40, 0), (-20, 0), (0, 0))
+
+position = [(-40, 0), (-20, 0), (0, 0)]
 snake = []
-window.tracer(0)
-for x in range(3):
+# window.tracer(0)
+for x in range(len(position)):
     turtle = Turtle("square")
     turtle.color("white")
     turtle.penup()
     turtle.goto(position[x])
     snake.append(turtle)
 
-while True:
-    for x in snake:
-        x.forward(0.1)
-    window.update()
+#* The snake move:
+# while True:
+#     for turtle in snake:
+#         turtle.forward(0.1)
+#         window.update()
 
+#* The snake move without tracer:
+while True:
+    for i in range(len(snake)-1):
+        snake[i].goto(snake[i+1].pos())
+    snake[-1].forward(20)
 
 window.exitonclick()
