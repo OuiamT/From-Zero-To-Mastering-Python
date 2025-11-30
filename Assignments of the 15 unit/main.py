9d533be2227f94cb4c76d73fdfcc292a7647ad10
OuiamT <tarikouiam24@gmail.com>
2025-11-30 13:38:31 +0100

    Some small codes about the movement of snake

--- Assignments of the 15 unit/main.py
+++ Assignments of the 15 unit/main.py
@@ -1,11 +1,15 @@
 from turtle import Turtle, Screen
+import random
+import time
+
 window = Screen()
 window.setup(width=600, height=600)
 window.bgcolor("black")
 
 position = [(-40, 0), (-20, 0), (0, 0)]
 snake = []
-# window.tracer(0)
+angles = (90, 0, 0, 0)
+window.tracer(0)
 for x in range(len(position)):
     turtle = Turtle("square")
     turtle.color("white")
@@ -14,15 +18,22 @@ for x in range(len(position)):
     snake.append(turtle)
 
 #* The snake move:
-# while True:
-#     for turtle in snake:
-#         turtle.forward(0.1)
-#         window.update()
+while True:
+    for turtle in snake:
+        turtle.forward(0.1)
+        window.update()
 
 #* The snake move without tracer:
-while True:
-    for i in range(len(snake)-1):
-        snake[i].goto(snake[i+1].pos())
-    snake[-1].forward(20)
+# while True:
+#     for i in range(len(snake)-1):
+#         snake[i].goto(snake[i+1].pos())
+#     snake[-1].forward(20)
 
-window.exitonclick()
+#* The snake move to the left:
+# while True:
+#     for i in range(len(snake)-1):
+#         snake[i].goto(snake[i+1].pos())
+#     snake[-1].forward(20)
+#     snake[-1].left(random.choice(angles))
+#     window.update()
+#     time.sleep(0.1)
