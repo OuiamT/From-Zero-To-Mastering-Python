4aa3b272cd993f57c58d0b8abe9d318ea43d26fd
OuiamT <tarikouiam24@gmail.com>
2025-12-26 17:12:05 +0100

    Knowlidgs about Pandas package

--- Assignments of the 19 unit/main.py
+++ Assignments of the 19 unit/main.py
@@ -1,1 +1,18 @@
-Commande ECHO activ‚e.
+import pandas as pd
+
+df = pd.read_csv("Workout.csv")
+
+# Convert the df to dictionary.
+print(df.to_dict())
+
+# Convert the 'day' column to list.
+print(df["day"].to_list())
+
+# Calculate the mean of the 'calories' column.
+# The classic methode:
+calories = df["burned_calories"].to_list()
+sum_of_calories = sum(calories)
+long_of_calories = len(calories)
+print(sum_of_calories / long_of_calories)
+# Use the function in pandas:
+print(df["burned_calories"].mean())
